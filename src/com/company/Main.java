ef21713a6331c9c72ca240ca5158cb70c8232732
Xyphrs <dromerof01@elpuig.xeill.net>
2021-02-02 11:36:54 +0100

    Test 2 git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mon");
         System.out.println("Test 2");
-        System.out.println("Test 1");
     }
 }
