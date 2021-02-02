53c8719b0d16fc03d40040ae9812eaeb273eb44a
Xyphrs <dromerof01@elpuig.xeill.net>
2021-02-02 11:37:20 +0100

    Commit Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola Mon");
-        System.out.println("Test 1");
+        System.out.println("Test 12");
     }
 }
