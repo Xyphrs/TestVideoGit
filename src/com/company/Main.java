4a47cc234b572b1cc168c1c11dcbca4ce0cac79a
Xyphrs <dromerof01@elpuig.xeill.net>
2021-02-02 12:54:45 +0100

    Commit Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,16 @@ package com.company;
 
 public class Main {
 
+    /**
+     * Main Method of Life.
+     * @param args Testing Javadoc.
+     */
     public static void main(String[] args) {
+
         System.out.println("Hola Mon");
         System.out.println("Test 2");
         System.out.println("Test 12222");
+
     }
+
 }
